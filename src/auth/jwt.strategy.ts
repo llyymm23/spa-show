05c83632490f6acb2db54c5bdc3a52708d708976
llyymm23 <jsy98bam1023@naver.com>
2024-06-18 23:20:13 +0900

    Fix : user 서비스 계층이 아닌 레포지토리에서 가져오기

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,11 +5,14 @@ import { UserService } from 'src/user/user.service';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(
-        private readonly userService: UserService,
+        @InjectRepository(User) private userRepository: Repository<User>,
         private readonly configService: ConfigService,
     ) {
         super({
@@ -20,7 +23,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: any) {
-        const user = await this.userService.findByEmail(payload.email);
+        const user = await this.userRepository.findOneBy({ email: payload.email });
+
         if (_.isNil(user)) {
             throw new NotFoundException('해당하는 사용자를 찾을 수 없습니다.');
         }
