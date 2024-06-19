9a0b0612b86711aa7d90ab3f823274957e4013d5
llyymm23 <jsy98bam1023@naver.com>
2024-06-19 16:02:33 +0900

    Update : 수정한 엔티티에 맞게 로직 변경, 공연 목록 조회와 이름 검색 api 합치기

--- src/show/show.controller.ts
+++ src/show/show.controller.ts
@@ -3,55 +3,57 @@ import { RolesGuard } from 'src/auth/roles.guard';
 import { Role } from 'src/user/types/userRole.type';
 
 import {
-    Body, Controller, Delete, Get, Param, Post, Put, Query, UploadedFile, UseGuards, UseInterceptors
+    Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, UploadedFile, UseGuards, UseInterceptors
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { CreateShowDto } from './dto/create-show.dto';
-import { SearchShowDTO } from './dto/search-show.dto';
 import { ShowService } from './show.service';
 import { AuthGuard } from '@nestjs/passport';
-import { FindAllShowDto } from './dto/find-all-show.dto';
+import { FindShowDto } from './dto/find-show.dto';
 
 @UseGuards(AuthGuard('jwt'))
 @UseGuards(RolesGuard)
 @Controller('show')
 export class ShowController {
-    constructor(private readonly showService: ShowService) { }
+    constructor(
+        private readonly showService: ShowService
+    ) { }
 
     //새 공연 등록
     //Admin만 가능
     @Roles(Role.Admin)
     @Post()
     async create(@Body() createshowDto: CreateShowDto) {
-        await this.showService.create(createshowDto);
+        const show = await this.showService.create(createshowDto);
+        return {
+            statusCode: HttpStatus.OK,
+            message: `새 공연을 등록했습니다.`,
+            show,
+        }
     }
 
-    //공연 목록 조회
+    //공연 검색하기(목록-키워드,카테고리/이름 검색)
     @Get()
-    async findAll(@Query() findAllShowDto: FindAllShowDto) {
-        return await this.showService.findAll(findAllShowDto);
+    async findShow(@Query() findShowDto: FindShowDto) {
+        const shows = await this.showService.findShow(findShowDto);
+        return {
+            statusCode: HttpStatus.OK,
+            message: `공연 검색에 성공하였습니다.`,
+            shows,
+        }
     }
 
     //공연 상세 보기
     @Get(':showId')
     async findOne(@Param('showId') showId: number) {
-        return await this.showService.findOne(showId);
+        const show = await this.showService.findOne(showId);
+        return {
+            statusCode: HttpStatus.OK,
+            message: `공연 상세 정보를 조회하였습니다.`,
+            show,
+        }
     }
-
-    //공연 검색하기
-    @Post('search')
-    async search(@Body() searchshowDto: SearchShowDTO) {
-        return await this.showService.search(searchshowDto);
-    }
-
-    //새 공연 등록
-    // @Roles(Role.Admin)
-    // @Post()
-    // @UseInterceptors(FileInterceptor('file'))
-    // async create(@UploadedFile() file: Express.Multer.File) {
-    //     await this.showService.create(file);
-    // }
 
     // //공연 수정
     // @Roles(Role.Admin)
--- src/show/show.service.ts
+++ src/show/show.service.ts
@@ -11,10 +11,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 //import { UpdateShowDto } from './dto/update-show.dto';
 import { Show } from './entities/show.entity';
-import { SearchShowDTO } from './dto/search-show.dto';
-import { CreateShowDto } from './dto/create-show.dto';
 import { Seat } from './entities/seat.entity';
-import { FindAllShowDto } from './dto/find-all-show.dto';
+import { CreateShowDto } from './dto/create-show.dto';
+import { FindShowDto } from './dto/find-show.dto';
 
 @Injectable()
 export class ShowService {
@@ -25,43 +24,55 @@ export class ShowService {
 
     //새 공연 등록하기
     async create(createshowDto: CreateShowDto) {
-        if (createshowDto.price > 50000) {
+        if (createshowDto.price.some(price => price > 50000)) {
             throw new BadRequestException('가격은 5만 포인트를 넘을 수 없습니다.');
         }
+
+        // date 필드에서 날짜와 시간을 분리
+        const dateObject = new Date(createshowDto.date);
+
+        // ISO 8601 형식에서 날짜 부분만 추출
+        const datePart = dateObject.toISOString().split('T')[0]; // 'YYYY-MM-DD'
+
+        // ISO 8601 형식에서 시간 부분만 추출
+        const timePart = dateObject.toTimeString().split(' ')[0]; // 'HH:mm:ss'
 
         //새 공연 등록
         const show = await this.showRepository.save({
             title: createshowDto.title,
             info: createshowDto.info,
+            date: datePart,
+            time: timePart,
             category: createshowDto.category,
             address: createshowDto.address,
             image: createshowDto.image,
-            price: createshowDto.price
+            reservationStart: createshowDto.reservationStart,
+            reservationEnd: createshowDto.reservationEnd,
         });
 
-        //시간 등록
-        const schedule = await this.scheduleRepository.save({
-            showId: show.showId,
-            date: createshowDto.date,
-            time: createshowDto.time
-        })
+        //좌석 등록
+        for (let i = 0; i < createshowDto.grade.length; i++) {
+            const grade = createshowDto.grade[i];
+            const price = createshowDto.price[i];
 
-        //좌석수 등록
-        await this.seatRepository.save({
-            scheduleId: schedule.scheduleId,
-            total_seat: createshowDto.total_seat,
-            current_seat: createshowDto.total_seat
-        })
+            for (let j = 1; j <= createshowDto.seat[i]; j++) {
+                await this.seatRepository.save({
+                    showId: show.showId,
+                    grade: grade,
+                    price: price,
+                    seatNum: j,
+                });
+            }
+        }
 
+        return show;
     }
 
-    //공연 목록 보기
-    // - 공연의 리스트를 조회합니다.
-    // - 전체, 공연명별로 나뉘어서 조회 가능합니다. 
-    async findAll(findAllShowDto: FindAllShowDto) {
-        const { keyword, category } = findAllShowDto;
+    //공연 검색하기(키워드,카테고리,이름 검색)
+    async findShow(findShowDto: FindShowDto) {
+        const { keyword, category, title } = findShowDto;
         const shows = await this.showRepository.find({
-            where: { ...(keyword && { title: Like(`%${keyword}%`) }), ...(category && { category }) },
+            where: { ...(keyword && { title: Like(`%${keyword}%`) }), ...(category && { category }), ...(title && { title }) },
         });
 
         return shows;
@@ -71,31 +82,11 @@ export class ShowService {
     // - 해당 공연의 정보를 반환합니다.
     // - 현재 예매가 가능한지 여부를 반환합니다.
     async findOne(showId: number) {
-        const show = await this.showRepository.findOne({
-            where: { showId },
-            relations: {
-                schedules: {
-                    seat: true,
-                }
-            }
-        });
-
-        console.log(show);
+        const show = await this.showRepository.findOne({ where: { showId } });
 
         if (_.isNil(show)) {
             throw new NotFoundException('존재하지 않는 공연입니다.');
         }
-
-        return show;
-    }
-
-    //공연 검색하기
-    async search(searchshowDto: SearchShowDTO) {
-        const { title } = searchshowDto;
-
-        const show = await this.showRepository.find({ where: { title } });
-
-        if (!show) { throw new NotFoundException('해당하는 공연이 없습니다.') };
 
         return show;
     }
