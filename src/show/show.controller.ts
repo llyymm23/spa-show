fb5418a356a1c0fa408b6ec35a5d7eacf95c959e
llyymm23 <jsy98bam1023@naver.com>
2024-06-19 23:38:48 +0900

    Fix : 엔티티 바뀐 부분 모두 반영해서 수정

--- src/show/show.controller.ts
+++ src/show/show.controller.ts
@@ -2,15 +2,13 @@ import { Roles } from 'src/auth/roles.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Role } from 'src/user/types/userRole.type';
 
-import {
-    Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, UploadedFile, UseGuards, UseInterceptors
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 
 import { CreateShowDto } from './dto/create-show.dto';
 import { ShowService } from './show.service';
 import { AuthGuard } from '@nestjs/passport';
 import { FindShowDto } from './dto/find-show.dto';
+import { UpdateShowDto } from './dto/update-show.dto';
 
 @UseGuards(AuthGuard('jwt'))
 @UseGuards(RolesGuard)
@@ -55,17 +53,29 @@ export class ShowController {
         }
     }
 
-    // //공연 수정
-    // @Roles(Role.Admin)
-    // @Put(':id')
-    // async update(@Param('id') id: number, @Body() updateshowDto: UpdateShowDto) {
-    //     await this.showService.update(id, updateTeamDto);
-    // }
+    //공연 내용 수정
+    //Admin만 가능
+    @Roles(Role.Admin)
+    @Put(':showId')
+    async updateShow(@Param('showId') showId: number, @Body() updateshowDto: UpdateShowDto) {
+        const show = await this.showService.updateShow(showId, updateshowDto);
+        return {
+            statusCode: HttpStatus.OK,
+            message: `공연 내용 수정에 성공하였습니다.`,
+            show,
+        }
+    }
 
-    // //공연 삭제
-    // @Roles(Role.Admin)
-    // @Delete(':id')
-    // async delete(@Param('id') id: number) {
-    //     await this.showService.delete(id);
-    // }
+    //공연 삭제
+    //Admin만 가능
+    @Roles(Role.Admin)
+    @Delete(':showId')
+    async deleteShow(@Param('showId') showId: number) {
+        const show = await this.showService.deleteShow(showId);
+        return {
+            statusCode: HttpStatus.OK,
+            message: `공연 삭제에 성공하였습니다.`,
+            show,
+        }
+    }
 }
--- src/show/show.service.ts
+++ src/show/show.service.ts
@@ -9,11 +9,11 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
-//import { UpdateShowDto } from './dto/update-show.dto';
 import { Show } from './entities/show.entity';
 import { Seat } from './entities/seat.entity';
 import { CreateShowDto } from './dto/create-show.dto';
 import { FindShowDto } from './dto/find-show.dto';
+import { UpdateShowDto } from './dto/update-show.dto';
 
 @Injectable()
 export class ShowService {
@@ -75,12 +75,14 @@ export class ShowService {
             where: { ...(keyword && { title: Like(`%${keyword}%`) }), ...(category && { category }), ...(title && { title }) },
         });
 
+        if (!shows) {
+            throw new NotFoundException('해당하는 공연이 존재하지 않습니다.');
+        }
+
         return shows;
     }
 
     //공연 상세 보기
-    // - 해당 공연의 정보를 반환합니다.
-    // - 현재 예매가 가능한지 여부를 반환합니다.
     async findOne(showId: number) {
         const show = await this.showRepository.findOne({ where: { showId } });
 
@@ -91,15 +93,31 @@ export class ShowService {
         return show;
     }
 
-    // //공연 수정하기
-    // async update(id: number, updateTeamDto: UpdateTeamDto) {
-    //     await this.verifyTeamById(id);
-    //     await this.teamRepository.update({ id }, updateTeamDto);
-    // }
+    //공연 수정하기
+    async updateShow(showId: number, updateshowDto: UpdateShowDto) {
+        const show = await this.showRepository.findOneBy({ showId });
 
-    // //공연 삭제하기
-    // async delete(id: number) {
-    //     await this.verifyTeamById(id);
-    //     await this.teamRepository.delete({ id });
-    // }
+        if (!show) {
+            throw new NotFoundException("해당 공연이 존재하지 않습니다.");
+        }
+
+        //주어진 updateshowDto로 전달된 값들만 기존의 데이터 업데이트
+        Object.assign(show, updateshowDto);
+
+        await this.showRepository.save(show);
+
+        return show;
+    }
+
+    //공연 삭제하기
+    async deleteShow(showId: number) {
+        const show = await this.showRepository.findOneBy({ showId });
+
+        if (!show) {
+            throw new NotFoundException("해당 공연이 존재하지 않습니다.");
+        }
+
+        await this.showRepository.delete({ showId });
+        return show;
+    }
 }
